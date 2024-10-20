fc0a0e57c1b6aa84615dcb16587eafbd9c893fe0
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-10-20 19:28:21 +0530

    Student Repository class code

---
+++ MainPackage/StudentDeo.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.Enity.Student;
+
+public interface StudentRepository extends CrudRepository<EntityclassName, Integer>{
+
+	
+
+}
