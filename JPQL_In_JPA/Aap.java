2da41c6d47bf13ff85f5bc30216a21942e356448
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-10-21 11:01:32 +0530

    Aap in java

---
+++ JPQL_In_JPA/Aap.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.demo.Enity.Student;
+
+@SpringBootApplication
+public class Project01Application {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(Project01Application.class, args);
+		StudentRepository repo = context.getBean(StudentRepository.class);
+
+		// Here you execute JPQL Query
+		 List<String> students =  repo.getStudents();
+		 students.forEach(e->{
+			 System.out.println(e);
+		 });
+		 
+		 // Here how get single Record.
+		 String student = repo.getStudentById(2);
+		 System.out.println(student);
+		 
+		 
+		
+
+		
+
+}
+
+
+}
