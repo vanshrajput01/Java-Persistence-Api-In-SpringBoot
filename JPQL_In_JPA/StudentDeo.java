ee16b19ab877d8d3c299fd0daba60ac27917fa2d
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-10-21 11:38:50 +0530

    Update StudentDeo.java

--- JPQL_In_JPA/StudentDeo.java
+++ JPQL_In_JPA/StudentDeo.java
@@ -10,6 +10,7 @@ import com.example.demo.Enity.Student;
 
 @Repository
 public interface StudentRepository extends CrudRepository<Student, Integer>{
+	#################################################################  JPQL ##############################################################################
 	############################################  how to get All data from database using JPQL ####################################################################### 
 	@Query("select s.studId,s.studFee,s.name from Student s")
 	public List<String> getStudents();
@@ -19,6 +20,15 @@ public interface StudentRepository extends CrudRepository<Student, Integer>{
 		
 	@Query("select s.studId,s.name,s.studFee from Student s where s.studId = :id")
 	public String getStudentById(@Param("id") Integer id);
+
+########################################################################### Native Query #########################################################################
+	
+	@Query(value = "select * from student_details",nativeQuery = true)
+	public List<Student> getAllStudent();
+
+	@Query(value = "select stud_id,name,stud_fee from student_details where name = :n",nativeQuery = true)
+	public List<Student> getStudentByName(@Param("n") String n);
+	
   
 	
 
